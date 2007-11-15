eff0d981128ad5717e69b7a0fdb9f0c4e499deb0
kemedes <kemedes@2752fbe2-5038-0410-9d0a-88578062bcef>
2007-11-15 17:35:54 +0000

    - Added lots of effects for testing on svn.

--- trunk/src/server/database/mysql/spells.sql
+++ trunk/src/server/database/mysql/spells.sql
@@ -34,10 +34,10 @@ CREATE TABLE spells (
 # Dumping data for table 'spells'
 #
 
-INSERT INTO spells VALUES("1","Summon Missile","1","1","crystal_casting","energy_arrow","","A wooden arrow is summoned and thrown at the target dealing 6*P damages.","1","cast Summon Missile","","0","-1","2","288","163","1.000");
-INSERT INTO spells VALUES("2","Life Infusion","1","1","test","test","","By means of this spell the wizard is able to instill pure energy in a creature. The energy, which has healing effects, is less powerful but similar to the energy of the Great Crystal. It can be cast on the wizard or on another character in touch range.","0","cast Life Infusion","","0","-1","5","24","164","1.000");
-INSERT INTO spells VALUES("3","Gust Of Wind","1","1","test","test","","AoE Example. A nasty smelling breeze passes by you.","0","cast Gust of Wind","","0","-1","20","32","165","1.500");
-INSERT INTO spells VALUES("4","Defensive Wind","1","1","test","test","","Buff Example. A calming effect washes over you.","0","cast Defensive Wind","","0","-1","50","24","166","1.500");
-INSERT INTO spells VALUES("5","Summon Creature","5","1","test","test","","The caster summons a creature as it's familiar.","0","cast Summon Creature","","0","-1","50","24","166","1.500");
-INSERT INTO spells VALUES("6","Fire Warts","3","1","red_casting","","","DoT Example. This spell will cause burning sores appear all over your enemies body, searing their flesh.","0","cast Fire Warts","","0","-1","20","288","165","1.500");
-INSERT INTO spells VALUES("7","Gem of Clarity","1","1","","","","Block Example. The caster will feel very clear minded.","0","GemOfClarity","","0","-1","20","8","165","1.500");
+INSERT INTO spells VALUES("1","Summon Missile","1","1","casting","test_","","A wooden arrow is summoned and thrown at the target dealing 6*P damages.","1","cast Summon Missile","","0","-1","2","288","163","1.000");
+INSERT INTO spells VALUES("2","Life Infusion","1","1","casting","clear","","By means of this spell the wizard is able to instill pure energy in a creature. The energy, which has healing effects, is less powerful but similar to the energy of the Great Crystal. It can be cast on the wizard or on another character in touch range.","0","cast Life Infusion","","0","-1","5","24","164","1.000");
+INSERT INTO spells VALUES("3","Gust Of Wind","1","1","casting","puff","","AoE Example. A nasty smelling breeze passes by you.","0","cast Gust of Wind","","0","-1","20","32","165","1.500");
+INSERT INTO spells VALUES("4","Defensive Wind","1","1","casting","puff","","Buff Example. A calming effect washes over you.","0","cast Defensive Wind","","0","-1","50","24","166","1.500");
+INSERT INTO spells VALUES("5","Summon Creature","5","1","casting","clear","","The caster summons a creature as it's familiar.","0","cast Summon Creature","","0","-1","50","24","166","1.500");
+INSERT INTO spells VALUES("6","Fire Warts","3","1","casting","fire","","DoT Example. This spell will cause burning sores appear all over your enemies body, searing their flesh.","0","cast Fire Warts","","0","-1","20","288","165","1.500");
+INSERT INTO spells VALUES("7","Gem of Clarity","1","1","casting","clear","","Block Example. The caster will feel very clear minded.","0","GemOfClarity","","0","-1","20","8","165","1.500");
