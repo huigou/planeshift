84ea134e97af5715656adc0b510e0b93f650b490
kemedes <kemedes@2752fbe2-5038-0410-9d0a-88578062bcef>
2007-12-02 19:51:16 +0000

    - Fixed FS#689.

--- src/server/psserverdr.cpp
+++ src/server/psserverdr.cpp
@@ -170,7 +170,16 @@ void psServerDR::HandleMessage (MsgEntry* me,Client *client)
 	}
 
     if ( !actor->IsAllowedToMove())  // Is this movement allowed?
+    {
 		client->FlagExploit();  // This DR data may be an exploit but may also be valid from lag.
+        if (drmsg.worldVel.y > 0)
+        {
+            actor->pcmove->AddVelocity(csVector3(0,-1,0));
+            actor->UpdateDR();
+            actor->MulticastDRUpdate();
+            return;
+        }
+    }
 
     if (drmsg.sector == NULL)
     {
