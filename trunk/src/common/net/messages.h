fd48996c0ef167565fd1b4de396ca5eea5f6dcca
magodra <magodra@2752fbe2-5038-0410-9d0a-88578062bcef>
2007-12-22 11:35:54 +0000

    Forgot to bump npc network version for this fix.

--- trunk/src/common/net/messages.h
+++ trunk/src/common/net/messages.h
@@ -48,7 +48,7 @@ struct iEngine;
 // NPC Networking version is separate so we don't have to break compatibility
 // with clients to enhance the superclients.  Made it a large number to ensure
 // no inadvertent overlaps.
-#define PS_NPCNETVERSION 0x100E
+#define PS_NPCNETVERSION 0x100F
 
 enum Slot_Containers
 {
