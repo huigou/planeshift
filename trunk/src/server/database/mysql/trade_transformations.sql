d9312a3602e05cebb47435cdc63af8a1c7745223
tomtt <tomtt@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-02-19 02:12:05 +0000

    Added some more test transformations and fixed some misspelling.

--- trunk/src/server/database/mysql/trade_transformations.sql
+++ trunk/src/server/database/mysql/trade_transformations.sql
@@ -15,7 +15,7 @@ CREATE TABLE trade_transformations (
   result_qty int(8) unsigned NOT NULL ,                         # resulting item quantity
   item_id int(10) unsigned NOT NULL,                         	# item to be transformed
   item_qty int(8) unsigned NOT NULL ,                           # required quantity for transformation
-  trans_points int(8) unsigned NOT NULL DEFAULT '0' ,           # amount of time to complete transformation
+  trans_points int(8) unsigned NOT NULL DEFAULT '0' ,           # ammount of time to complete transformation
   penilty_pct float(10,6) NOT NULL DEFAULT '1.000000' ,     # percent of quality for resulting item 
   description varchar(255) NOT NULL DEFAULT '' ,
   PRIMARY KEY (id)
@@ -31,10 +31,10 @@ INSERT INTO trade_transformations VALUES(2,1,2,66,1,77,1,10,0.600000,'You mix wa
 INSERT INTO trade_transformations VALUES(3,1,2,67,1,78,1,10,0.800000,'You mix waybread sweet slop into sweet batter in mixing bowl.');
 INSERT INTO trade_transformations VALUES(4,1,2,68,1,79,1,10,1.000000,'You mix waybread superb slop into superb batter in mixing bowl.');
 INSERT INTO trade_transformations VALUES(5,1,3,69,1,76,1,10,0.400000,'You mix waybread slop into batter in mixing bowl with mixer.');
-INSERT INTO trade_transformations VALUES(6,2,5,134,1,69,1,20,0.400000,'You kneed waybread batter into dought on table.');
-INSERT INTO trade_transformations VALUES(7,2,5,135,1,66,1,20,0.600000,'You kneed nutty waybread batter into nutty dought on table.');
-INSERT INTO trade_transformations VALUES(8,2,5,136,1,67,1,20,0.800000,'You kneed sweet waybread batter into sweet dought on table.');
-INSERT INTO trade_transformations VALUES(9,2,5,137,1,68,1,20,1.000000,'You kneed superb waybread batter into superb dought on table.');
+INSERT INTO trade_transformations VALUES(6,2,5,134,1,69,1,20,0.400000,'You kneed waybread batter into dough on table.');
+INSERT INTO trade_transformations VALUES(7,2,5,135,1,66,1,20,0.600000,'You kneed nutty waybread batter into nutty dough on table.');
+INSERT INTO trade_transformations VALUES(8,2,5,136,1,67,1,20,0.800000,'You kneed sweet waybread batter into sweet dough on table.');
+INSERT INTO trade_transformations VALUES(9,2,5,137,1,68,1,20,1.000000,'You kneed superb waybread batter into superb dough on table.');
 INSERT INTO trade_transformations VALUES(10,1,4,80,1,134,1,20,0.400000,'You bake waybread dough into waybread in oven.');
 INSERT INTO trade_transformations VALUES(11,1,4,81,1,135,1,20,0.600000,'You bake nutty waybread dough into nutty waybread in oven.');
 INSERT INTO trade_transformations VALUES(12,1,4,82,1,136,1,20,0.800000,'You bake sweet waybread dough into sweet waybread in oven.');
@@ -53,5 +53,7 @@ INSERT INTO trade_transformations VALUES(24,3,8,111,1,110,1,20,1.000000,'You ham
 INSERT INTO trade_transformations VALUES(25,3,8,112,1,111,2,20,1.000000,'You hammer two short iron bars into an iron bar.');
 INSERT INTO trade_transformations VALUES(26,3,8,113,1,112,2,20,1.000000,'You hammer two iron bars into a long iron bar.');
 INSERT INTO trade_transformations VALUES(27,3,9,121,1,113,1,20,1.000000,'You cast a fishing reel from long iron bar.');
-#INSERT INTO trade_transformations VALUES(28,1,0,75,1,0,0,40,0.010000,'You make some useless stuff.');
-#INSERT INTO trade_transformations VALUES(29,1,0,75,0,75,0,40,0.010000,'You make some useless stuff.');
+INSERT INTO trade_transformations VALUES(28,6,10,69,1,76,1,1,0.400000,'You mix wayoutbread slop into batter in mixing bowl.');
+INSERT INTO trade_transformations VALUES(29,6,11,134,1,69,1,1,0.400000,'You kneed wayoutbread batter into dough.');
+INSERT INTO trade_transformations VALUES(30,6,11,80,1,134,1,1,0.400000,'You bake wayoutbread dough into wayoutbread.');
+INSERT INTO trade_transformations VALUES(31,7,12,73,1,0,0,1,1.000000,'You bake anything and it turns into dust.');
