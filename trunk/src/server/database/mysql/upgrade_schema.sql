2bd7812d5afe738c7fdd0f899feba3aacfa0d862
mgist <mgist@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-03-20 14:03:58 +0000

    - Added missing db version bump line to upgrade_schema.sql

--- trunk/src/server/database/mysql/upgrade_schema.sql
+++ trunk/src/server/database/mysql/upgrade_schema.sql
@@ -890,6 +890,7 @@ UPDATE `server_options` SET `option_value`='1181' WHERE `option_name`='db_versio
 update item_instances set loc_instance=0x7fffffff where loc_instance=-1;
 ALTER TABLE item_instances MODIFY COLUMN `loc_instance` int(11) unsigned default '0';
 update item_instances set loc_instance=0xffffffff where loc_instance=0x7fffffff;
+UPDATE `server_options` SET `option_value`='1182' WHERE `option_name`='db_version';
 
 # Insert your upgrade before this line. Remember when you set a new db_version
 # to update the server_options.sql file and update psserver.cpp as well.
