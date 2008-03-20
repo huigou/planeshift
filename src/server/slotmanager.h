3d6d473d3e5b451816589816947d8d1fcf815cfc
tomtt <tomtt@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-03-20 00:16:33 +0000

    Added math script for quality affect for consuming scripts.

--- src/server/slotmanager.h
+++ src/server/slotmanager.h
@@ -32,6 +32,9 @@
 //=============================================================================
 #include "msgmanager.h"
 
+class MathScript;
+class MathScriptVar;
+
 /** Handles Drag and Drop Messages from the client.
  *  This basically figures out what item is being dragged and places it in a
  *  holding slot.  It then looks for the destination slot and sees if it can be placed
