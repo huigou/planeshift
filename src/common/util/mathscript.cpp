37e761277f57c4a077886c715a86a19192822404
khakilord <khakilord@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-04-14 02:17:47 +0000

    Fixed buffer overrun issue with CalculateMaxHP script by disabling optimization on all scripts.

--- src/common/util/mathscript.cpp
+++ src/common/util/mathscript.cpp
@@ -221,8 +221,6 @@ void MathScriptLine::ParseFormula(MathScript *myScript)
         printf( "Caller: %s", myScript->name.GetData());
         Error4("Error in Col %d: %s\n Script: \"%s\" ",(int)ret,fp.ErrorMsg(),scriptLine.GetData() );
     }
-    else
-        fp.Optimize();
 }
 
 void MathScriptLine::Execute()
