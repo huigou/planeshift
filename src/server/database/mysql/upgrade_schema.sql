96e7f6dd9c1caa928680b3b7d31332d2022eee5d
eriroley <eriroley@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-04-24 01:18:41 +0000

    - Added table wc_accessrules in preparation for updated WC - DB VER BUMP

--- src/server/database/mysql/upgrade_schema.sql
+++ src/server/database/mysql/upgrade_schema.sql
@@ -900,6 +900,9 @@ ALTER TABLE item_categories ADD COLUMN identify_skill_id int(10);
 ALTER TABLE item_categories ADD COLUMN identify_min_skill int(8) unsigned;
 UPDATE `server_options` SET `option_value`='1183' WHERE `option_name`='db_version';
 
+#### 1184 - Frank Barton - Added wc_accessrules table in prep for updated WC
+CREATE TABLE `wc_accessrules` (`id` int(10) NOT NULL auto_increment, `security_level` tinyint(3) NOT NULL, `objecttype` varchar(50) NOT NULL, `access` tinyint(3) default NULL, UNIQUE KEY `id` (`id`))
+
 # Insert your upgrade before this line. Remember when you set a new db_version
 # to update the server_options.sql file and update psserver.cpp as well.
 # This to ensure that everything is working if you use the create_all.sql to
