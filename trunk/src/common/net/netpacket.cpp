1e8cc5e15b52edb664945e4fb3c3e36c54b68f70
khakilord <khakilord@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-05-03 16:49:39 +0000

    Fixed accidental commit

--- trunk/src/common/net/netpacket.cpp
+++ trunk/src/common/net/netpacket.cpp
@@ -31,7 +31,6 @@ psNetPacketEntry::psNetPacketEntry (psNetPacket* packet, uint32_t cnum,
 {
     packet->pktsize = sz - sizeof(psNetPacket);
     timestamp = csGetTicks();
-    printf("Constructed1 %p\n", this);
 }
 
 /** construct a new PacketEntry for a single or partial message */
@@ -51,7 +50,6 @@ psNetPacketEntry::psNetPacketEntry (uint8_t pri, uint32_t cnum,
     timestamp = csGetTicks();
     if (msg && sz && sz != PKTSIZE_ACK)
         memcpy(packet->data, ((char *)msg) + off, sz);
-    printf("Constructed2 %p\n", this);
 }
 
 psNetPacketEntry::psNetPacketEntry (uint8_t pri, uint32_t cnum,
@@ -69,14 +67,12 @@ psNetPacketEntry::psNetPacketEntry (uint8_t pri, uint32_t cnum,
     timestamp = csGetTicks();
     if (bytes && sz && sz != PKTSIZE_ACK)
     memcpy(packet->data, bytes, sz);
-    printf("Constructed3 %p\n", this);
 }
 
 psNetPacketEntry::~psNetPacketEntry()
 {
     if (packet)
         cs_free(packet);
-    printf("Destructed %p\n", this);
 }
 
 bool psNetPacketEntry::Append(psNetPacketEntry *next)
