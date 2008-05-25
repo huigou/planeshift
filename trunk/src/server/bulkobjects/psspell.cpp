d42689e1c07d4833e0bfc207c08d3082925d1074
Khakilord <Khakilord@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-05-25 13:10:21 +0000

    Added assertions to try to ensure the offensive flag is set correctly. Fixed some offensive flags in the db spells.

--- trunk/src/server/bulkobjects/psspell.cpp
+++ trunk/src/server/bulkobjects/psspell.cpp
@@ -89,13 +89,13 @@ bool psSpell::Load(iResultRow& row)
     csString errorMsg;
     if(!offensive)
     {
-        errorMsg.Format("Non-offensive spell '%s' cannot be cast on friends!", (const char *) name);
-        CS_ASSERT_MSG(errorMsg, (spell_target & TARGET_FRIEND) > 0);
+        errorMsg.Format("Non-offensive spell '%s' cannot be cast on friends/self!", (const char *) name);
+        CS_ASSERT_MSG(errorMsg, (spell_target & TARGET_FRIEND) > 0 || (spell_target & TARGET_SELF) > 0);
     }
     else
     {
         errorMsg.Format("Offensive spell '%s' cannot be cast on enemies!", (const char *) name);
-        CS_ASSERT_MSG(errorMsg, (spell_target & TARGET_PVP) > 0);
+        CS_ASSERT_MSG(errorMsg, (spell_target & TARGET_PVP) > 0 || (spell_target & TARGET_FOE) > 0);
     }
 
     npcSpellCategoryID    = row.GetInt("cstr_npc_spell_category");
