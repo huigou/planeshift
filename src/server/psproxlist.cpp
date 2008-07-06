bc3a632c6399d57ca3f4831bb8efe9392b0fb352
mgist <mgist@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-07-06 08:00:40 +0000

    - Reverted 1845 to attempt a proper fix.

--- src/server/psproxlist.cpp
+++ src/server/psproxlist.cpp
@@ -394,10 +394,9 @@ void ProximityList::ClearTouched()
 {
     size_t objNum;
 
-    // Element 0 will always be 'self', and so will always be touched.
-    for (objNum = 1; objNum < objectsThatWatchMe_touched.GetSize(); objNum++ )
+    for (objNum = 0; objNum < objectsThatWatchMe_touched.GetSize(); objNum++ )
         objectsThatWatchMe_touched[objNum] = false;
-    for (objNum = 1; objNum < objectsThatIWatch_touched.GetSize(); objNum++ )
+    for (objNum = 0; objNum < objectsThatIWatch_touched.GetSize(); objNum++ )
         objectsThatIWatch_touched[objNum]  = false;
 }
 
