6a1a78048b6f1972ca9913db0a511139d375a691
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-10-31 14:08:31 +0000

    Removed the APPNAME define from psengine.cpp so the variable is fetched from pscssetup.h and changing it only there updates also the client version shown in the stdout

--- trunk/src/client/psengine.cpp
+++ trunk/src/client/psengine.cpp
@@ -40,7 +40,6 @@ MiniDumper minidumper;
 
 //////////////////////////////////////////////////////////////////////////////
 
-#define APPNAME              "PlaneShift Steel Blue (0.4.03)"
 #define CONFIGFILENAME       "/planeshift/userdata/planeshift.cfg"
 #define PSAPP                "planeshift.application.client"
 
