08f453ffeafdb365873b06ef3ef47acff9f9e48c
Vengeance2001 <Vengeance2001@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-11-26 06:19:17 +0000

    DB version bump

--- src/server/psserver.cpp
+++ src/server/psserver.cpp
@@ -89,7 +89,7 @@
 #include "introductionmanager.h"
 
 // Remember to bump this in server_options.sql and add to upgrade_schema.sql!
-#define DATABASE_VERSION_STR "1207"
+#define DATABASE_VERSION_STR "1208"
 
 
 psCharacterLoader psServer::CharacterLoader;
