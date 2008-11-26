7787d75d0aae123e3261e4dcb60d9ff4c78d69cf
Vengeance2001 <Vengeance2001@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-11-26 05:40:48 +0000

    DB version bump

--- src/server/database/mysql/quests.sql
+++ src/server/database/mysql/quests.sql
@@ -1,58 +1,99 @@
-# MySQL-Front Dump 1.16 beta
+# HeidiSQL Dump 
 #
-# Host: localhost Database: planeshift
-#--------------------------------------------------------
-# Server version 4.0.18-max-nt
+# --------------------------------------------------------
+# Host:                 127.0.0.1
+# Database:             planeshift
+# Server version:       5.0.67-community-nt
+# Server OS:            Win32
+# Target-Compatibility: Standard ANSI SQL
+# HeidiSQL version:     3.2 Revision: 1129
+# --------------------------------------------------------
+
+/*!40100 SET CHARACTER SET latin1;*/
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ANSI';*/
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;*/
+
+
+#
+# Database structure for database 'planeshift'
+#
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ "planeshift" /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE "planeshift";
+
+
 #
 # Table structure for table 'quests'
 #
 
-CREATE TABLE quests (
-  id int(10) unsigned NOT NULL auto_increment,
-  name varchar(40) NOT NULL DEFAULT '' ,
-  task varchar(250) NOT NULL DEFAULT '' ,
-  cstr_id_icon int(10) unsigned DEFAULT '0' ,
-  flags tinyint(3) unsigned DEFAULT '0' ,
-  master_quest_id int(10) unsigned ,
-  minor_step_number tinyint(3) unsigned DEFAULT '0' ,
-  player_lockout_time int(10) NOT NULL DEFAULT '0' ,
-  quest_lockout_time int(10) NOT NULL DEFAULT '0' ,
-  category varchar(30) NOT NULL DEFAULT '' ,
-  prerequisite varchar(250) NOT NULL DEFAULT '' ,
-  PRIMARY KEY (id)
-);
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "quests" (
+  "id" int(10) unsigned NOT NULL auto_increment,
+  "name" varchar(40) NOT NULL default '',
+  "task" varchar(250) NOT NULL default '',
+  "cstr_id_icon" int(10) unsigned default '0',
+  "flags" tinyint(3) unsigned default '0',
+  "master_quest_id" int(10) unsigned default NULL,
+  "minor_step_number" tinyint(3) unsigned default '0',
+  "player_lockout_time" int(10) NOT NULL default '0',
+  "quest_lockout_time" int(10) NOT NULL default '0',
+  "category" varchar(30) NOT NULL default '',
+  "prerequisite" varchar(250) NOT NULL default '',
+  PRIMARY KEY  ("id"),
+  UNIQUE KEY "indx_quests_name" ("name")
+) AUTO_INCREMENT=208 /*!40100 DEFAULT CHARSET=utf8*/;
+
 
 
 #
 # Dumping data for table 'quests'
 #
 
-INSERT INTO quests VALUES("1","Rescue the Princess","Princess Leia is being held by the Empire on the Death Star.  You must rescue her before she is compelled to give up the wherabouts of the rebel base!","0","0","0","0","0","0","Newbie","");
-INSERT INTO quests VALUES("2","Sandwich Quest","Bring the merchant a freakin' sandwich!","0","0","0","0","10","0","Newbie","");
-INSERT INTO quests VALUES("3","Falchion Quest","Bring the merchant a steel falchion","0","0","0","0","120","60","Newbie","<pre><skill name=\"Sword\" min=\"10\" max=\"20\"/></pre>");
-INSERT INTO quests VALUES("10","Male Enki Alina Quest","Find Smith and ask him if he loves the daugther.","0","0","0","0","240","60","Newbie","<pre><completed quest=\"Male Enki Gold\"/></pre>");
-INSERT INTO quests VALUES("11","Male Enki Gold","Mine gold ore for the MaleEnki npc.","0","0","0","0","240","60","Newbie","");
-INSERT INTO quests VALUES("12","Male Enki Trusted Transport","Transport the glyph to the merchant.","0","0","0","0","300","60","Newbie","<pre><or><completed quest=\"Male Enki Gold\"/><completed quest=\"Male Enki Alina Quest\"/></or></pre>");
-INSERT INTO quests VALUES("13","Male Enki Lost Found","Get Key","0","0","0","0","300","60","Newbie","");
-INSERT INTO quests VALUES("14","Flying a Kite","Up up and a way on a windy day!","0","0","0","0","1","1","Newbie",'<pre><activemagic name="+DefensiveWind"/></pre>');
-INSERT INTO quests VALUES("15","Reverse Word Lookup", "Tests checking each word in a phrase as as trigger", 0, 0, 0, 0, 1, 1, "Newbie", "");
-INSERT INTO quests VALUES("16","Multiple item test", "Tests giving multiple items and money at the same time", 0, 0, 0, 0, 1, 1, "Newbie", "");
-
-#
-# Quest Test Case quests
-#
-# QuestMaster1 Quests
-#
-INSERT INTO quests VALUES("101","QuestMaster1 Quest 1","Test Quest for the Quest Test Case: Quest sub step control","0","0","0","0","0","0","Test1","");
-INSERT INTO quests VALUES("102","QuestMaster1 Quest 2","Test Quest for the Quest Test Case: Quest lockouts(player lockout 30 sec.)","0","0","0","0","30","0","Test1","");
-INSERT INTO quests VALUES("103","QuestMaster1 Quest 3","Test Quest for the Quest Test Case: Quest lockouts(quest lockout 30 sec.)","0","0","0","0","0","30","Test1","");
-INSERT INTO quests VALUES("104","QuestMaster1 Quest 4","Test Quest for the Quest Test Case: Give items","0","0","0","0","0","30","Test1","");
-#
-# QuestMaster2 Quests
-#
-INSERT INTO quests VALUES("201","QuestMaster2 Quest 1","Test Quest for the Quest Test Case: Quest prerequisite","0","0","0","0","0","0","Test2","");
-INSERT INTO quests VALUES("202","QuestMaster2 Quest 2","Test Quest for the Quest Test Case: Quest prerequisite","0","0","0","0","0","0","Test2","<pre><and><completed quest=\"QuestMaster2 Quest 1\"/><not><completed quest=\"QuestMaster2 Quest 3\"/></not></and></pre>");
-INSERT INTO quests VALUES("203","QuestMaster2 Quest 3","Test Quest for the Quest Test Case: Quest prerequisite","0","0","0","0","0","0","Test2","<pre><or><completed quest=\"QuestMaster2 Quest 1\"/><completed quest=\"QuestMaster2 Quest 2\"/></or></pre>");
-INSERT INTO quests VALUES("204","QuestMaster2 Quest 4","Test Quest for the Quest Test Case: Quest prerequisite","0","0","0","0","0","0","Test2","<pre><require min=\"2\"><completed quest=\"QuestMaster2 Quest 1\"/><completed quest=\"QuestMaster2 Quest 2\"/><completed quest=\"QuestMaster2 Quest 3\"/></require></pre>");
-INSERT INTO quests VALUES("205","QuestMaster2 Quest 5","Test Quest for the Quest Test Case: Quest prerequisite","0","0","0","0","0","0","Test2","<pre><completed category=\"Test2\" min=\"3\" max=\"4\"/></pre>");
-INSERT INTO quests VALUES("206","QuestMaster2 Quest 6","Test Quest for the Quest Test Case: Quest lockouts","0","0","0","0","-1","0","Test3","");
+LOCK TABLES "quests" WRITE;
+/*!40000 ALTER TABLE "quests" DISABLE KEYS;*/
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('1','Rescue the Princess','Princess Leia is being held by the Empire on the Death Star.  You must rescue her before she is compelled to give up the wherabouts of the rebel base!','0',0,'0',0,0,0,'Newbie','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('2','Sandwich Quest','Bring the merchant a freakin'' sandwich!','0',0,'0',0,10,0,'Newbie','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('3','Falchion Quest','Bring the merchant a steel falchion','0',0,'0',0,120,60,'Newbie','<pre><skill name="Sword" min="10" max="20"/></pre>');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('10','Male Enki Alina Quest','Find Smith and ask him if he loves the daugther.','0',0,'0',0,240,60,'Newbie','<pre><completed quest="Male Enki Gold"/></pre>');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('11','Male Enki Gold','Mine gold ore for the MaleEnki npc.','0',0,'0',0,240,60,'Newbie','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('12','Male Enki Trusted Transport','Transport the glyph to the merchant.','0',0,'0',0,300,60,'Newbie','<pre><or><completed quest="Male Enki Gold"/><completed quest="Male Enki Alina Quest"/></or></pre>');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('13','Male Enki Lost Found','Get Key','0',0,'0',0,300,60,'Newbie','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('14','Flying a Kite','Up up and a way on a windy day!','0',0,'0',0,1,1,'Newbie','<pre><activemagic name="+DefensiveWind"/></pre>');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('15','Reverse Word Lookup','Tests checking each word in a phrase as as trigger','0',0,'0',0,1,1,'Newbie','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('16','Multiple item test','Tests giving multiple items and money at the same time','0',0,'0',0,1,1,'Newbie','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('101','QuestMaster1 Quest 1','Test Quest for the Quest Test Case: Quest sub step control','0',0,'0',0,0,0,'Test1','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('102','QuestMaster1 Quest 2','Test Quest for the Quest Test Case: Quest lockouts(player lockout 30 sec.)','0',0,'0',0,30,0,'Test1','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('103','QuestMaster1 Quest 3','Test Quest for the Quest Test Case: Quest lockouts(quest lockout 30 sec.)','0',0,'0',0,0,30,'Test1','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('104','QuestMaster1 Quest 4','Test Quest for the Quest Test Case: Give items','0',0,'0',0,0,30,'Test1','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('201','QuestMaster2 Quest 1','Test Quest for the Quest Test Case: Quest prerequisite','0',0,'0',0,0,0,'Test2','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('202','QuestMaster2 Quest 2','Test Quest for the Quest Test Case: Quest prerequisite','0',0,'0',0,0,0,'Test2','<pre><and><completed quest="QuestMaster2 Quest 1"/><not><completed quest="QuestMaster2 Quest 3"/></not></and></pre>');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('203','QuestMaster2 Quest 3','Test Quest for the Quest Test Case: Quest prerequisite','0',0,'0',0,0,0,'Test2','<pre><or><completed quest="QuestMaster2 Quest 1"/><completed quest="QuestMaster2 Quest 2"/></or></pre>');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('204','QuestMaster2 Quest 4','Test Quest for the Quest Test Case: Quest prerequisite','0',0,'0',0,0,0,'Test2','<pre><require min="2"><completed quest="QuestMaster2 Quest 1"/><completed quest="QuestMaster2 Quest 2"/><completed quest="QuestMaster2 Quest 3"/></require></pre>');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('205','QuestMaster2 Quest 5','Test Quest for the Quest Test Case: Quest prerequisite','0',0,'0',0,0,0,'Test2','<pre><completed category="Test2" min="3" max="4"/></pre>');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('206','QuestMaster2 Quest 6','Test Quest for the Quest Test Case: Quest lockouts','0',0,'0',0,-1,0,'Test3','');
+REPLACE INTO "quests" ("id", "name", "task", "cstr_id_icon", "flags", "master_quest_id", "minor_step_number", "player_lockout_time", "quest_lockout_time", "category", "prerequisite") VALUES
+	('207','Acquire Lapar','Get the things he wants to get your Lapar','0',0,NULL,0,0,0,'TestAudio','');
+/*!40000 ALTER TABLE "quests" ENABLE KEYS;*/
+UNLOCK TABLES;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE;*/
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;*/
