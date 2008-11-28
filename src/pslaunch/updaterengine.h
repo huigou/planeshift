0612ff191154dbdc25084db2736d6027907803ae
mgist <mgist@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-11-28 16:41:41 +0000

    - Bumped updater version.

--- src/pslaunch/updaterengine.h
+++ src/pslaunch/updaterengine.h
@@ -34,7 +34,7 @@
 #include "util/singleton.h"
 
 /* To be incremented every time we want to make an update. */
-#define UPDATER_VERSION 1.1
+#define UPDATER_VERSION 1.2
 
 struct iConfigManager;
 struct iVFS;
