0eacba606e7e40f3e09deac6eb409f3888baddcd
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-12-25 16:26:11 +0000

    a little wrong thing

--- trunk/src/server/exchangemanager.cpp
+++ trunk/src/server/exchangemanager.cpp
@@ -1374,7 +1374,7 @@ void ExchangeManager::StartExchange( Client* client, bool withPlayer )
         return;
     }
 
-    if(client->IsFrozen())
+    if(target->IsFrozen())
     {
         psserver->SendSystemInfo(client->GetClientNum(), "% was frozen by a GM and cannot trade", target->GetName());
         return;
