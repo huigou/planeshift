d889dc3b664a29f72960fc14036567d46085bcf2
Vengeance2001 <Vengeance2001@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-01-06 17:50:31 +0000

    Fixes and updates to npc spawn ranges table.  Thanks to Aiken for doing most of the legwork here.  DB VERSION BUMP.

--- src/server/psserver.cpp
+++ src/server/psserver.cpp
@@ -89,7 +89,7 @@
 #include "introductionmanager.h"
 
 // Remember to bump this in server_options.sql and add to upgrade_schema.sql!
-#define DATABASE_VERSION_STR "1211"
+#define DATABASE_VERSION_STR "1212"
 
 
 psCharacterLoader psServer::CharacterLoader;
