992e645c5960d498d5541c9f627521a1b1f82ca1
mr_dave <mr_dave@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-01-06 17:37:29 +0000

    Progression scripts support for minigames

--- src/server/database/mysql/gameboards.sql
+++ src/server/database/mysql/gameboards.sql
@@ -31,7 +31,7 @@ CREATE TABLE gameboards
 #
 # Notes for gameboards.endgames
 # <MGEndGame>
-#  <EndGame Coords="relative"/"absolute" [SourceTile="T"]>
+#  <EndGame Coords="relative"/"absolute" [SourceTile="T"] [Winner="T"]>
 #   <Coord Col="0-15" Row="0-15" Tile="T" />
 #  </EndGame>
 # </MGEndGame>
@@ -44,4 +44,5 @@ CREATE TABLE gameboards
 INSERT INTO `gameboards` VALUES ('Test Game', 6, 6, 'FF00FFF0000F000000000000F0000FFF00FF', '123456789ABCDE', 2, 'White,Checked', '', '');
 INSERT INTO `gameboards` VALUES ('Test Game 2', 6, 6, 'FF00FFF0000F000000000000F0000FFF00FF', '123456789ABCDE', 1, 'White,Checked', '', '');
 INSERT INTO `gameboards` VALUES ('Tic Tac Toe', 3, 3, '000000000', '12', 2, 'White,Plain', '<GameRules><Rules PlayerTurns=\"StrictOrdered\" MoveType=\"PlaceOnly\" MoveTo=\"Vacancy\" /></GameRules>', '<MGEndGame><EndGame Coords=\"relative\" SourceTile=\"A\"><Coord Col=\"1\" Row=\"0\" Tile=\"F\" /><Coord Col=\"2\" Row=\"0\" Tile=\"F\" /></EndGame><EndGame Coords=\"relative\" SourceTile=\"A\"><Coord Col=\"0\" Row=\"1\" Tile=\"F\" /><Coord Col=\"0\" Row=\"2\" Tile=\"F\" /></EndGame><EndGame Coords=\"relative\" SourceTile=\"A\"><Coord Col=\"1\" Row=\"1\" Tile=\"F\" /><Coord Col=\"2\" Row=\"2\" Tile=\"F\" /></EndGame><EndGame Coords=\"relative\" SourceTile=\"A\"><Coord Col=\"-1\" Row=\"1\" Tile=\"F\" /><Coord Col=\"-2\" Row=\"2\" Tile=\"F\" /></EndGame></MGEndGame>');
+INSERT INTO `gameboards` VALUES ('Weather game', 3, 3, '000000000', '1', 1, 'Black,Plain', '', '<MGEndGame><EndGame Coords=\"absolute\"><Coord Col=\"0\" Row=\"0\" Tile=\"A\" /><Coord Col=\"1\" Row=\"0\" Tile=\"A\" /><Coord Col=\"2\" Row=\"0\" Tile=\"A\" /><Coord Col=\"0\" Row=\"1\" Tile=\"A\" /><Coord Col=\"0\" Row=\"2\" Tile=\"A\" /></EndGame></MGEndGame>');
 
