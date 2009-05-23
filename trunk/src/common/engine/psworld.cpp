274e3d1cc68c57133a9c65af61ba68d5130da8ba
kougaro <kougaro@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-05-23 10:35:40 +0000

    oops?

--- trunk/src/common/engine/psworld.cpp
+++ trunk/src/common/engine/psworld.cpp
@@ -371,7 +371,7 @@ float psWorld::Matrix2YRot(const csMatrix3& mat)
     csVector3 vec(0,0,1);
     vec = mat * vec;
 
-    float result = atan2(vec.z, vec.x);
+    float result = atan2(vec.x, vec.z);
     // force the result in range [0;2*pi]
     if(result < 0) result += TWO_PI;
 
