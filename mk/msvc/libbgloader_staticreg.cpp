e59301a15f1610e8a8216d317b7555dcf9bb237f
psshared <psshared@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-05-24 15:54:44 +0000

    Visual-C++ project files: Automated Visual-C++ project file repair.

--- mk/msvc/libbgloader_staticreg.cpp
+++ mk/msvc/libbgloader_staticreg.cpp
@@ -15,7 +15,40 @@
 
 namespace csStaticPluginInit
 {
+#include "cssysdef.h"
+#include "csutil/scf.h"
+
+// Put static linking stuff into own section.
+// The idea is that this allows the section to be swapped out but not
+// swapped in again b/c something else in it was needed.
+#if !defined(CS_DEBUG) && defined(CS_COMPILER_MSVC)
+#pragma const_seg(".CSmetai")
+#pragma comment(linker, "/section:.CSmetai,r")
+#pragma code_seg(".CSmeta")
+#pragma comment(linker, "/section:.CSmeta,er")
+#pragma comment(linker, "/merge:.CSmetai=.CSmeta")
+#endif
+
 static char const metainfo_bgloader[] =
+namespace csStaticPluginInit
+{
+static char const metainfo_bgloader[] =
+"<?xml version=\"1.0\"?>"
+"<plugin>"
+"  <scf>"
+"    <classes>"
+"      <class>"
+"        <name>crystalspace.bgloader</name>"
+"        <implementation>BgLoader</implementation>"
+"        <description>Background Loader</description>"
+"        <requires>"
+"          <class>crystalspace.level.threadedloader</class>"
+"        </requires>"
+"      </class>"
+"    </classes>"
+"  </scf>"
+"</plugin>"
+;
 "<?xml version=\"1.0\"?>"
 "<plugin>"
 "  <scf>"
@@ -37,9 +70,27 @@ static char const metainfo_bgloader[] =
     SCF_DEFINE_FACTORY_FUNC_REGISTRATION(BgLoader) 
   #endif
 
+  #ifndef BgLoader_FACTORY_REGISTER_DEFINED 
+  #define BgLoader_FACTORY_REGISTER_DEFINED 
+    SCF_DEFINE_FACTORY_FUNC_REGISTRATION(BgLoader) 
+  #endif
+
 class bgloader
 {
 SCF_REGISTER_STATIC_LIBRARY(bgloader,metainfo_bgloader)
+class bgloader
+{
+SCF_REGISTER_STATIC_LIBRARY(bgloader,metainfo_bgloader)
+  #ifndef BgLoader_FACTORY_REGISTERED 
+  #define BgLoader_FACTORY_REGISTERED 
+    BgLoader_StaticInit BgLoader_static_init__; 
+  #endif
+public:
+ bgloader();
+};
+bgloader::bgloader() {}
+
+}
   #ifndef BgLoader_FACTORY_REGISTERED 
   #define BgLoader_FACTORY_REGISTERED 
     BgLoader_StaticInit BgLoader_static_init__; 
