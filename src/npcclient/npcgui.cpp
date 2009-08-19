6490e9418eecd693d16985665dd96e73ba2f9c04
mgist <mgist@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-08-19 19:15:12 +0000

    - More npcclient gui skeleton code.

--- src/npcclient/npcgui.cpp
+++ src/npcclient/npcgui.cpp
@@ -1,3 +1,22 @@
+/*
+* pslaunch.h - Author: Mike Gist
+*
+* Copyright (C) 2009 Atomic Blue (info@planeshift.it, http://www.atomicblue.org)
+*
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation (version 2 of the License)
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
+
 #include <psconfig.h>
 
 #include <cstool/initapp.h>
@@ -64,8 +83,21 @@ bool NpcGui::Initialise()
   mainWidget = new pawsMainWidget();
   paws->SetMainWidget(mainWidget);
 
+  // Register factory
+  guiWidget = new pawsNPCClientWindowFactory();
+
   // Load and assign a default button click sound for pawsbutton
   paws->LoadSound("/planeshift/art/sounds/gui/next.wav","sound.standardButtonClick");
+
+  // Load widgets
+  if (!paws->LoadWidget("data/gui/npcclient.xml"))
+  {
+    printf("Warning: Loading 'data/gui/npcclient.xml' failed!");
+    return false;
+  }
+
+  pawsWidget* npcclientWidget = paws->FindWidget("NPCClient");
+  npcclientWidget->SetBackgroundAlpha(0);
 
   // Set mouse image.
   paws->GetMouse()->ChangeImage("Standard Mouse Pointer");
@@ -143,4 +175,8 @@ void NpcGui::FrameLimit()
     csSleep(sleeptime - elapsedTime);
 
   elapsed = csGetTicks();
+}
+
+pawsNPCClientWindow::pawsNPCClientWindow()
+{
 }
--- src/npcclient/npcgui.h
+++ src/npcclient/npcgui.h
@@ -1,8 +1,36 @@
+/*
+* pslaunch.h - Author: Mike Gist
+*
+* Copyright (C) 2009 Atomic Blue (info@planeshift.it, http://www.atomicblue.org)
+*
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation (version 2 of the License)
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
+
+#ifndef __NPCGUI_H__
+#define __NPCGUI_H__
+
 #include "util/genericevent.h"
+
+#include "paws/pawswidget.h"
 
 class EventHandler;
 class PawsManager;
+class pawsComboBox;
+class pawsNPCClientWindowFactory;
 class pawsMainWidget;
+class pawsOkBox;
+class pawsYesNoBox;
 class psNPCClient;
 struct iGraphics2D;
 struct iGraphics3D;
@@ -42,4 +70,19 @@ private:
 
   /* Time when the last frame was drawn. */
   csTicks elapsed;
+
+  /* Widget for npcclient gui. */
+  pawsNPCClientWindowFactory* guiWidget;
 };
+
+class pawsNPCClientWindow : public pawsWidget
+{
+public:
+  pawsNPCClientWindow();
+
+private:  
+};
+
+CREATE_PAWS_FACTORY( pawsNPCClientWindow );
+
+#endif // __NPCGUI_H__
