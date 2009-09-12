2d3a5bffe2762805565ad6e3a3985e4c68758fd7
Vengeance2001 <Vengeance2001@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-09-12 05:17:39 +0000

    Mouse options window now displays.  Haven't tested functionality but battery going dead here.

--- trunk/src/client/gui/pawsconfigmouse.cpp
+++ trunk/src/client/gui/pawsconfigmouse.cpp
@@ -136,20 +136,25 @@ bool pawsConfigMouse::CreateTree()
 {
     pawsTreeNode * root;
 
-    tree = new pawsTree();
-    tree->MoveTo(screenFrame.xmin, screenFrame.ymin);
-    tree->SetSize(screenFrame.Width(), screenFrame.Height());
-    tree->SetScrollBars(false, true);
-    tree->SetTreeLayout(new pawsStdTreeLayout(tree, 5, 20));
-    tree->SetTreeDecorator(new pawsStdTreeDecorator(tree, graphics2D, 0x0000ff, 0x00ffff, 13));
-    AddChild(tree);
+//    tree = new pawsTree();
+//    tree->MoveTo(screenFrame.xmin, screenFrame.ymin);
+//    tree->SetSize(screenFrame.Width(), screenFrame.Height());
+//    tree->SetScrollBars(false, true);
+//    tree->SetTreeLayout(new pawsStdTreeLayout(tree, 5, 20));
+//    tree->SetTreeDecorator(new pawsStdTreeDecorator(tree, graphics2D, 0x0000ff, 0x00ffff, 13));
+//    AddChild(tree);
 
-    if ( ! tree->LoadFromFile("configmouse.xml"))
+    if (!LoadFromFile("configmouse.xml"))
         return false;
+
+    tree = dynamic_cast<pawsTree *>(children[0]);
+    tree->SetRelativeFrameSize(parent->ScreenFrame().Width(), parent->ScreenFrame().Height());
 
     root = tree->GetRoot();
     if (root != NULL)
         CreateTreeWidgets(root);
+
+    tree->SetScrollBars(false,true);
 
     return true;
 }
