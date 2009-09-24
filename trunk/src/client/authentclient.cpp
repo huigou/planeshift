b5a940abc712010c2940347cbfc03d156674f754
Khakilord <Khakilord@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-09-24 17:53:09 +0000

    Fixed order of os/gfxcard

--- trunk/src/client/authentclient.cpp
+++ trunk/src/client/authentclient.cpp
@@ -190,7 +190,7 @@ void psAuthenticationClient::HandlePreAuth( MsgEntry* me )
     //TODO: convert this to use csstrings
     // Get os and graphics card info
     iGraphics2D *graphics2D = PawsManager::GetSingleton().GetGraphics3D()->GetDriver2D();
-    psAuthenticationMessage request(0,username.GetData(), hexstring.GetData(), graphics2D->GetHWRenderer(), CS_PLATFORM_NAME "-" CS_PROCESSOR_NAME "-" CS_COMPILER_NAME );
+    psAuthenticationMessage request(0,username.GetData(), hexstring.GetData(), CS_PLATFORM_NAME "-" CS_PROCESSOR_NAME "-" CS_COMPILER_NAME, graphics2D->GetHWRenderer() );
     
     request.SendMessage();                
 }
