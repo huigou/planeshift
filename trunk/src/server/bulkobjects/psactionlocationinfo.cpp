bf6dcabc9c8f2e9b1c3c35fa33dd711ada4da8aa
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-10-12 09:39:26 +0000

    string litteral passed directly to command/commandpump

--- trunk/src/server/bulkobjects/psactionlocationinfo.cpp
+++ trunk/src/server/bulkobjects/psactionlocationinfo.cpp
@@ -423,9 +423,9 @@ unsigned int psActionLocation::Insert( const char *table, const char **fieldname
             command.Append("NULL");
         }
     }
-    command.Append(")");
+    command.Append(")");	
 
-    if (db->Command(command)!=1)
+    if (db->Command("%s", command)!=1)
         return 0;
 
     return db->GetLastInsertID();
@@ -469,7 +469,7 @@ bool psActionLocation::UpdateByKey( const char *table, const char *idname, const
     command.Append(escape);
     command.Append("'");
 
-    if (db->Command(command)==QUERY_FAILED)
+    if (db->Command("%s", command)==QUERY_FAILED)
     {
         return false;
     }
@@ -492,7 +492,7 @@ bool psActionLocation::DeleteByKey( const char *table, const char *idname, const
     command.Append(escape);
     command.Append("'");
 
-    if (db->Command(command)==QUERY_FAILED)
+    if (db->Command("%s", command)==QUERY_FAILED)
     {
         return false;
     }
