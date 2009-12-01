aaa11abc3531c964ffc71bd7bc4dfd9d46b3f155
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-12-01 10:09:34 +0000

    reverting for now there could be an issue with custom names in items

--- trunk/src/common/paws/pawsborder.cpp
+++ trunk/src/common/paws/pawsborder.cpp
@@ -355,7 +355,7 @@ void DrawBumpFrame( iGraphics2D * graphics2D, pawsWidget * widget, csRect frame,
 
 void pawsBorder::SetTitle(const char *t, bool shadow)
 {
-    title = PawsManager::GetSingleton().Translate(t);
+    title = t;
     shadowFont = shadow;
 }
 
