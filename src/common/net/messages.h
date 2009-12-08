21834cd7f0472aa456ae98221920165653735f81
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-12-08 08:08:46 +0000

    netbump 0xB5

--- src/common/net/messages.h
+++ src/common/net/messages.h
@@ -45,7 +45,7 @@ class csStringHashReversible;
 
 // This holds the version number of the network code, remember to increase
 // this each time you do an update which breaks compatibility
-#define PS_NETVERSION   0x00B4
+#define PS_NETVERSION   0x00B5
 // Remember to bump the version in pscssetup.h, as well.
 
 
