85461dbd28ccf6caaa22cc230c29974f17bf1be0
kennygraunke <kennygraunke@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-12-27 03:50:54 +0000

    Made give_exp tell the player how much exp they got.
    Quests use this to award experience.

--- src/server/database/mysql/progress_events.sql
+++ src/server/database/mysql/progress_events.sql
@@ -22,7 +22,7 @@ CREATE TABLE progression_events (
 INSERT INTO progression_events VALUES("respawn",'<script><teleport aim="Target" location="spawn"/></script>');
 INSERT INTO progression_events VALUES("go to tic tac toe",'<script><teleport aim="Target" sector="NPCroom" x="-1.69" y="2.89" z="-217.21"/></script>');
 INSERT INTO progression_events VALUES("run to the hills",'<script><teleport aim="Target" sector="npcroom2"/></script>');
-INSERT INTO progression_events VALUES("give_exp",'<script><exp aim="Target" value="Exp"/></script>');
+INSERT INTO progression_events VALUES("give_exp",'<script><exp aim="Target" value="Exp"/><msg aim="Target" text="You gained ${Exp} experience."/></script>');
 INSERT INTO progression_events VALUES("cast Life Infusion",
     '<script>
        <hp aim="Target" value="10*Power"/>
