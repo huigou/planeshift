b00367498cf8f680fbae0cadd14be904c95d7dac
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-06-09 19:43:29 +0000

    added a server api for use by outside server account creation

---
+++ tools/serverapi.php
@@ -0,0 +1,21 @@
+<?php
+// Quest dumper from db to txt files
+
+$ip = "127.0.0.1";
+$code = "abcd";
+
+// Change your database settings here:
+$db = mysql_connect("localhost", "planeshift", "planeshift") or die ("Unable to connect!"); // host, user, pass
+mysql_select_db("planeshift", $db) or die("Could not select database");
+
+echo $_GET["pass"];
+echo $_GET["user"];
+if($_SERVER["REMOTE_ADDR"] != $ip || $_GET["code"] != $code)
+{
+	echo "Access DENIED!";
+	exit();
+}
+
+mysql_query("INSERT into accounts (username,password,last_login_ip,security_level,status) VALUES\n(\"".$_GET["user"]."\",\"".$_GET["pass"]."\",\"127.0.0.1\", 0, \"A\")") or die("fatal error: couldn't insert stuff: " . mysql_error() . "\n");
+
+?>
