18306e0fddfdc4090ce7685ae44ba809c80e92b7
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-09-25 15:05:12 +0000

    fixed an xml parsing bug which was there since ever, removed useless old code and optimized the randomization overlay generation by using an hash.

--- src/server/lootrandomizer.h
+++ src/server/lootrandomizer.h
@@ -27,16 +27,16 @@ class RandomizedOverlay;
  */
 struct LootModifier
 {
-    uint32_t id; ///< The id assigned to this modifier in the db. It's referenced by psitem.
-    csString modifier_type;
-    csString name;
-    csString effect;
-    csString equip_script;
-    float    probability;
-    csString stat_req_modifier;
-    float    cost_modifier;
-    csString mesh;
-    csString not_usable_with;
+    uint32_t id;                ///< The id assigned to this modifier in the db. It's referenced by psitem.
+    csString modifier_type;     ///< The type of modifier (suffix, prefix, adjective)
+    csString name;              ///< The part of name which this modifier will add when used. The position is determined by modifier_type
+    csString effect;            ///< Declares modifiers to some stats like weight, weapon speed, resistance/attack types.
+    csString equip_script;      ///< The part of equip_script to add to the item when this modifier is used.
+    float    probability;       ///< The probability for this loot modifier to happen when generating a random item.
+    csString stat_req_modifier; ///< The additional requirements to stats when this modifier is used.
+    float    cost_modifier;     ///< The cost of this modifier @see CalcModifierCostCap
+    csString mesh;              ///< The mesh this modifier will use for the random item generated.
+    csString not_usable_with;   ///< Defines which modifiers this isn't usable with.
 };
 
 class MathScript;
@@ -47,9 +47,10 @@ class MathScript;
 class LootRandomizer
 {
 protected:
-    csArray<LootModifier*> prefixes;
-    csArray<LootModifier*> suffixes;
-    csArray<LootModifier*> adjectives;
+    csArray<LootModifier*> prefixes;   ///< Keeps all the loot modifiers of type "prefix"
+    csArray<LootModifier*> suffixes;   ///< Keeps all the loot modifiers of type "suffix"
+    csArray<LootModifier*> adjectives; ///< Keeps all the loot modifiers of type "adjective"
+    csHash<LootModifier*, uint32_t> LootModifiersById;  ///< Keeps all the lootmodifiers for faster access when we know the id.
 
     // precalculated max values for probability. min is always 0
     float prefix_max;
@@ -57,21 +58,39 @@ protected:
     float suffix_max;
 
 public:
+    /** Constructor.
+     *  @param cachemanager A pointer to the cache manager.
+     */
     LootRandomizer(CacheManager* cachemanager);
+    ///Destructor
     ~LootRandomizer();
 
     /// This adds another item to the entries array
     void AddLootModifier( LootModifier *entry );
     
+    /** Gets a loot modifier from it's id.
+     *  @param id The id of the item we are searching for.
+     *  @return A pointer to the loot modifier which is referenced by the id we were searching for.
+     */
     LootModifier *GetModifier(uint32_t id);
 
-    /// This randomizes the current loot item and returns the item with the modifiers applied
+    /** This randomizes the current loot item and returns the item with the modifiers applied.
+     *  @param item The item instance which we will be randomizing.
+     *  @param cost The maximum "cost" of the randomization we can apply @see CalcModifierCostCap
+     *  @param lootTesting Says if we really are applying the modifiers.
+     *  @param numModifiers Forces the amount of modifiers to apply.
+     */
     psItem* RandomizeItem( psItem* item,
                                 float cost,
                                 bool lootTesting = false,
                                 size_t numModifiers = 0 );
 
     float CalcModifierCostCap(psCharacter *chr);
+    /** Applies modifications to a randomized overlay depending on the requested ids.
+     *  @param baseItem The basic item which will have the overlay generated for.
+     *  @param overlay A pointer to the overlay where we will save the modifications to apply to this item.
+     *  @param modifiersIds An array with all the ids of the modifiers which we will need to apply to the overlay.
+     */
     void ApplyModifier(psItemStats* baseItem, RandomizedOverlay* overlay, csArray<uint32_t>& modifiersIds);
 
 protected:
@@ -80,9 +99,10 @@ protected:
 
 private:
     void AddModifier( LootModifier *oper1, LootModifier *oper2 );
-    /** sets an attribute to the item overlay. utility function used when parsing the loot modifiers xml */
+    /** sets an attribute to the item overlay. utility function used when parsing the loot modifiers xml
+     *  @param overlay The randomization overlay where we are applying these attributes.
+     */
     bool SetAttribute(const csString & op, const csString & attrName, float modifier, RandomizedOverlay* overlay);
-   // void ApplyModifier(psItemStats *loot, LootModifier *mod);
 };
 
 #endif
