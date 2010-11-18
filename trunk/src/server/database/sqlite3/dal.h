b07a95b235f2c50539b42746c96e0252e2e6a4a2
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-11-18 09:27:41 +0000

    identation fixes

--- trunk/src/server/database/sqlite3/dal.h
+++ trunk/src/server/database/sqlite3/dal.h
@@ -29,270 +29,270 @@ struct iObjectRegistry;
 
 CS_PLUGIN_NAMESPACE_BEGIN(dbsqlite3)
 {
-#ifdef USE_DELAY_QUERY
-#define THREADED_BUFFER_SIZE 300
-class DelayedQueryManager : public CS::Threading::Runnable
-{
-public:
-    DelayedQueryManager(const char *host, unsigned int port, const char *database,
-                              const char *user, const char *pwd);
-
-    virtual void Run ();
-    void Push(csString query);
-    void Stop();
-private:
-    sqlite3* m_conn;
-    csString arr[THREADED_BUFFER_SIZE];
-    size_t start, end;
-    CS::Threading::Mutex mutex;
-    CS::Threading::RecursiveMutex mutexArray;
-    CS::Threading::Condition datacondition;
-    bool m_Close;
-    psDBProfiles profs;
-    csString m_host;
-    unsigned int m_port;
-    csString m_db;
-    csString m_user;
-    csString m_pwd;
-};
-#endif
-
-class psMysqlConnection : public scfImplementation2<psMysqlConnection, iComponent, iDataConnection>
-{
-protected:
-    sqlite3 *conn;  //Points to mydb after a successfull connection to the db
-    csString lastquery;
-    iObjectRegistry *objectReg;
-    psDBProfiles profs;
-    csString profileDump;
-    LogCSV* logcsv;
-
-public:
-    psMysqlConnection(iBase *iParent);
-    virtual ~psMysqlConnection();
-
-    bool Initialize (iObjectRegistry *objectreg);
-    bool Initialize(const char *host, unsigned int port, const char *database, 
-                    const char *user, const char *pwd, LogCSV* logcsv);
-    virtual bool Close();
-
-    int IsValid(void);
-
-    /** Escapes a string to safely insert it into the database.
-     *  @param to Where the resulting escaped string will be placed.
-     *  @param from The string that we want to escape.
-     */
-    void Escape(csString& to, const char *from);
-    
-    iResultSet *Select(const char *sql,...);
-    int SelectSingleNumber(const char *sql, ...);
-    unsigned long Command(const char *sql,...);
-    unsigned long CommandPump(const char *sql,...);
-
-    uint64 GenericInsertWithID(const char *table,const char **fieldnames,psStringArray& fieldvalues);
-    bool GenericUpdateWithID(const char *table,const char *idfield,const char *id,const char **fieldnames,psStringArray& fieldvalues);
-    bool GenericUpdateWithID(const char *table,const char *idfield,const char *id,psStringArray& fields);
-
-    const char *GetLastError(void);
-    const char *GetLastQuery(void)
+    #ifdef USE_DELAY_QUERY
+    #define THREADED_BUFFER_SIZE 300
+    class DelayedQueryManager : public CS::Threading::Runnable
     {
-        return lastquery;
+    public:
+        DelayedQueryManager(const char *host, unsigned int port, const char *database,
+                                  const char *user, const char *pwd);
+
+        virtual void Run ();
+        void Push(csString query);
+        void Stop();
+    private:
+        sqlite3* m_conn;
+        csString arr[THREADED_BUFFER_SIZE];
+        size_t start, end;
+        CS::Threading::Mutex mutex;
+        CS::Threading::RecursiveMutex mutexArray;
+        CS::Threading::Condition datacondition;
+        bool m_Close;
+        psDBProfiles profs;
+        csString m_host;
+        unsigned int m_port;
+        csString m_db;
+        csString m_user;
+        csString m_pwd;
     };
-    uint64 GetLastInsertID();
-    
-    const char *uint64tostring(uint64 value,csString& recv);
+    #endif
 
-    virtual const char* DumpProfile();
-    virtual void ResetProfile();
-    
-    iRecord* NewUpdatePreparedStatement(const char* table, const char* idfield, unsigned int count, const char* file, unsigned int line);
-    iRecord* NewInsertPreparedStatement(const char* table, unsigned int count, const char* file, unsigned int line);
-
-#ifdef USE_DELAY_QUERY    
-    csRef<DelayedQueryManager> dqm;
-    csRef<Thread> dqmThread;
-#endif    
-};
-
-
-class psResultRow : public iResultRow
-{
-protected:
-    int rowNum;
-    char **rs;
-    int max;
-
-public:
-    psResultRow()
+    class psMysqlConnection : public scfImplementation2<psMysqlConnection, iComponent, iDataConnection>
     {
-        rowNum = 0;
-        rs = NULL;
-    };
+    protected:
+        sqlite3 *conn;  //Points to mydb after a successfull connection to the db
+        csString lastquery;
+        iObjectRegistry *objectReg;
+        psDBProfiles profs;
+        csString profileDump;
+        LogCSV* logcsv;
 
-    void SetMaxFields(int fields)  {    max = fields;   };
-    void SetResultSet(void* resultsettoken);
+    public:
+        psMysqlConnection(iBase *iParent);
+        virtual ~psMysqlConnection();
 
-    int Fetch(int row);
+        bool Initialize (iObjectRegistry *objectreg);
+        bool Initialize(const char *host, unsigned int port, const char *database, 
+                        const char *user, const char *pwd, LogCSV* logcsv);
+        virtual bool Close();
 
-    const char *operator[](int whichfield);
-    const char *operator[](const char *fieldname);
+        int IsValid(void);
 
-    int GetInt(int whichfield);
-    int GetInt(const char *fieldname);
-
-    unsigned long GetUInt32(int whichfield);
-    unsigned long GetUInt32(const char *fieldname);
-
-    float GetFloat(int whichfield);
-    float GetFloat(const char *fieldname);
-
-    uint64 GetUInt64(int whichfield);
-    uint64 GetUInt64(const char *fieldname);
-
-    uint64 stringtouint64(const char *stringbuf);
-    const char *uint64tostring(uint64 value,char *stringbuf,int buflen);
-};
-
-class psResultSet : public iResultSet
-{
-protected:
-    char **rs;
-    unsigned long rows, fields, current;
-    psResultRow  row;
-
-public:
-    psResultSet(char **result, int rows, int columns);
-    virtual ~psResultSet();
-
-    void Release(void) { delete this; };
-
-    iResultRow& operator[](unsigned long whichrow);
-
-    unsigned long Count(void) { return rows; };
-};
-
-class dbRecord : public iRecord
-{
-protected:
-    enum FIELDTYPE { SQL_TYPE_FLOAT, SQL_TYPE_INT, SQL_TYPE_STRING, SQL_TYPE_NULL };
-
-    typedef struct
-    {
-        float fValue;
-        int iValue;
-        csString sValue;
-        FIELDTYPE type;
-    } StatField;
-    const char* table;
-    const char* idfield;
-    
-    sqlite3* conn;
-    
-    psStringArray command;
-    bool prepared;
-    
-    sqlite3_stmt* stmt;
-    
-    unsigned int index;
-    unsigned int count;
-    
-    // Useful for debugging
-    LogCSV* logcsv;
-    const char* file;
-    unsigned int line;
-    
-    StatField* temp;
-    
-    virtual void AddToStatement(const char* fname) = 0;
-    
-    virtual void SetID(uint32 uid) = 0;
-    
-public:
-    dbRecord(sqlite3* db, const char* Table, const char* Idfield, unsigned int count, LogCSV* logcsv, const char* file, unsigned int line)
-    {
-        conn = db;
-        table = Table;
-        idfield = Idfield;
-        temp = new StatField[count];
-        index = 0;
-        this->count = count;
-        this->logcsv = logcsv;
-        this->file = file;
-        this->line = line;
+        /** Escapes a string to safely insert it into the database.
+         *  @param to Where the resulting escaped string will be placed.
+         *  @param from The string that we want to escape.
+         */
+        void Escape(csString& to, const char *from);
         
-        stmt = NULL;
-        prepared = false;
-    }
-    
-    virtual ~dbRecord()
+        iResultSet *Select(const char *sql,...);
+        int SelectSingleNumber(const char *sql, ...);
+        unsigned long Command(const char *sql,...);
+        unsigned long CommandPump(const char *sql,...);
+
+        uint64 GenericInsertWithID(const char *table,const char **fieldnames,psStringArray& fieldvalues);
+        bool GenericUpdateWithID(const char *table,const char *idfield,const char *id,const char **fieldnames,psStringArray& fieldvalues);
+        bool GenericUpdateWithID(const char *table,const char *idfield,const char *id,psStringArray& fields);
+
+        const char *GetLastError(void);
+        const char *GetLastQuery(void)
+        {
+            return lastquery;
+        };
+        uint64 GetLastInsertID();
+        
+        const char *uint64tostring(uint64 value,csString& recv);
+
+        virtual const char* DumpProfile();
+        virtual void ResetProfile();
+        
+        iRecord* NewUpdatePreparedStatement(const char* table, const char* idfield, unsigned int count, const char* file, unsigned int line);
+        iRecord* NewInsertPreparedStatement(const char* table, unsigned int count, const char* file, unsigned int line);
+
+    #ifdef USE_DELAY_QUERY    
+        csRef<DelayedQueryManager> dqm;
+        csRef<Thread> dqmThread;
+    #endif    
+    };
+
+
+    class psResultRow : public iResultRow
     {
-        sqlite3_finalize(stmt);
-        delete[] temp;
-    }
-    
-    void Reset()
+    protected:
+        int rowNum;
+        char **rs;
+        int max;
+
+    public:
+        psResultRow()
+        {
+            rowNum = 0;
+            rs = NULL;
+        };
+
+        void SetMaxFields(int fields)  {    max = fields;   };
+        void SetResultSet(void* resultsettoken);
+
+        int Fetch(int row);
+
+        const char *operator[](int whichfield);
+        const char *operator[](const char *fieldname);
+
+        int GetInt(int whichfield);
+        int GetInt(const char *fieldname);
+
+        unsigned long GetUInt32(int whichfield);
+        unsigned long GetUInt32(const char *fieldname);
+
+        float GetFloat(int whichfield);
+        float GetFloat(const char *fieldname);
+
+        uint64 GetUInt64(int whichfield);
+        uint64 GetUInt64(const char *fieldname);
+
+        uint64 stringtouint64(const char *stringbuf);
+        const char *uint64tostring(uint64 value,char *stringbuf,int buflen);
+    };
+
+    class psResultSet : public iResultSet
     {
-        index = 0;
-        command.Empty(); //clears the command array to avoid restarting from a wrong position.
-        if(stmt)
-            sqlite3_reset(stmt);        
-    }
+    protected:
+        char **rs;
+        unsigned long rows, fields, current;
+        psResultRow  row;
 
-    
-    void AddField(const char* fname, float fValue);
-    
-    void AddField(const char* fname, int iValue);
-    
-    void AddField(const char* fname, unsigned int uiValue);
-    
-    void AddField(const char* fname, unsigned short usValue);
+    public:
+        psResultSet(char **result, int rows, int columns);
+        virtual ~psResultSet();
 
-    void AddField(const char* fname, const char* sValue);
+        void Release(void) { delete this; };
 
-    void AddFieldNull(const char* fname);
-    
-    virtual bool Prepare() = 0;
+        iResultRow& operator[](unsigned long whichrow);
 
-    virtual bool Execute(uint32 uid);
-};
+        unsigned long Count(void) { return rows; };
+    };
 
-class dbInsert : public dbRecord
-{
-    virtual void AddToStatement(const char* fname)
+    class dbRecord : public iRecord
     {
-        if(!prepared)
-            command.Push(fname);
-    }
-    
-    virtual void SetID(uint32 uid)  {  };
-    
-public:
-    dbInsert(sqlite3* db, const char* Table, unsigned int count, LogCSV* logcsv, const char* file, unsigned int line)
-    : dbRecord(db, Table, "", count, logcsv, file, line) { }
-    
-    virtual bool Prepare();
-};
+    protected:
+        enum FIELDTYPE { SQL_TYPE_FLOAT, SQL_TYPE_INT, SQL_TYPE_STRING, SQL_TYPE_NULL };
 
-class dbUpdate : public dbRecord
-{
-    virtual void AddToStatement(const char* fname)
-    {
-        if(!prepared)
-            command.FormatPush("%s = ?", fname);
-    }
-    
-    virtual void SetID(uint32 uid) 
-    {
-        temp[index].iValue = uid;
-        temp[index].type = SQL_TYPE_INT;
-        index++;
-    }
-public:
-    dbUpdate(sqlite3* db, const char* Table, const char* Idfield, unsigned int count, LogCSV* logcsv, const char* file, unsigned int line)
-    : dbRecord(db, Table, Idfield, count, logcsv, file, line) { }
-    virtual bool Prepare();
+        typedef struct
+        {
+            float fValue;
+            int iValue;
+            csString sValue;
+            FIELDTYPE type;
+        } StatField;
+        const char* table;
+        const char* idfield;
+        
+        sqlite3* conn;
+        
+        psStringArray command;
+        bool prepared;
+        
+        sqlite3_stmt* stmt;
+        
+        unsigned int index;
+        unsigned int count;
+        
+        // Useful for debugging
+        LogCSV* logcsv;
+        const char* file;
+        unsigned int line;
+        
+        StatField* temp;
+        
+        virtual void AddToStatement(const char* fname) = 0;
+        
+        virtual void SetID(uint32 uid) = 0;
+        
+    public:
+        dbRecord(sqlite3* db, const char* Table, const char* Idfield, unsigned int count, LogCSV* logcsv, const char* file, unsigned int line)
+        {
+            conn = db;
+            table = Table;
+            idfield = Idfield;
+            temp = new StatField[count];
+            index = 0;
+            this->count = count;
+            this->logcsv = logcsv;
+            this->file = file;
+            this->line = line;
+            
+            stmt = NULL;
+            prepared = false;
+        }
+        
+        virtual ~dbRecord()
+        {
+            sqlite3_finalize(stmt);
+            delete[] temp;
+        }
+        
+        void Reset()
+        {
+            index = 0;
+            command.Empty(); //clears the command array to avoid restarting from a wrong position.
+            if(stmt)
+                sqlite3_reset(stmt);        
+        }
 
-};
+        
+        void AddField(const char* fname, float fValue);
+        
+        void AddField(const char* fname, int iValue);
+        
+        void AddField(const char* fname, unsigned int uiValue);
+        
+        void AddField(const char* fname, unsigned short usValue);
+
+        void AddField(const char* fname, const char* sValue);
+
+        void AddFieldNull(const char* fname);
+        
+        virtual bool Prepare() = 0;
+
+        virtual bool Execute(uint32 uid);
+    };
+
+    class dbInsert : public dbRecord
+    {
+        virtual void AddToStatement(const char* fname)
+        {
+            if(!prepared)
+                command.Push(fname);
+        }
+        
+        virtual void SetID(uint32 uid)  {  };
+        
+    public:
+        dbInsert(sqlite3* db, const char* Table, unsigned int count, LogCSV* logcsv, const char* file, unsigned int line)
+        : dbRecord(db, Table, "", count, logcsv, file, line) { }
+        
+        virtual bool Prepare();
+    };
+
+    class dbUpdate : public dbRecord
+    {
+        virtual void AddToStatement(const char* fname)
+        {
+            if(!prepared)
+                command.FormatPush("%s = ?", fname);
+        }
+        
+        virtual void SetID(uint32 uid) 
+        {
+            temp[index].iValue = uid;
+            temp[index].type = SQL_TYPE_INT;
+            index++;
+        }
+    public:
+        dbUpdate(sqlite3* db, const char* Table, const char* Idfield, unsigned int count, LogCSV* logcsv, const char* file, unsigned int line)
+        : dbRecord(db, Table, Idfield, count, logcsv, file, line) { }
+        virtual bool Prepare();
+
+    };
 
 } CS_PLUGIN_NAMESPACE_END(dbsqlite3)   
 #endif
