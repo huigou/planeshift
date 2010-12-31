2828a62b90e27f28a2bbd48b75a3c6da5a265442
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-12-31 13:41:02 +0000

    make clear() clear also the bartenderaction

--- src/client/gui/pawsslot.cpp
+++ src/client/gui/pawsslot.cpp
@@ -123,7 +123,7 @@ bool pawsSlot::OnMouseDown( int button, int modifiers, int x, int y )
         if((modifiers & CSMASK_CTRL) && (modifiers & CSMASK_ALT))
         {
             Clear();
-            action.Empty();
+            clearBartenderAction();
             return true;
         }
         //if we aren't pressing ctrl we execute the command else we allow drag and drop
@@ -269,6 +269,7 @@ void pawsSlot::Clear()
     stackCountLabel->Hide();
     SetPurifyStatus(0);
     SetToolTip("");
+    clearBartenderAction();
 
     reserved = false;
     image = NULL;
