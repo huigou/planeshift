22aceab7fb80ac73e91df04032e3df0a33d59dcf
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2011-07-22 14:43:56 +0000

    added creation dates to gm events and alliances patch by Cameron Clausen

--- trunk/src/server/database/mysql/gm_events.sql
+++ trunk/src/server/database/mysql/gm_events.sql
@@ -4,6 +4,7 @@
 DROP TABLE IF EXISTS `gm_events`;
 CREATE TABLE `gm_events` (
   `id` int(10) NOT NULL default '0',
+  `date_created` TIMESTAMP default current_timestamp,
   `name` varchar(40) NOT NULL default '',
   `description` blob NOT NULL,
   `status` int NOT NULL default '0',
