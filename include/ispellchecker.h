951327ef418245be4ce915edbb312e8b09acfefb
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2011-08-02 18:31:27 +0000

    fixes for copy constructors and improved comments patch by aiwendilh

--- include/ispellchecker.h
+++ include/ispellchecker.h
@@ -1,7 +1,7 @@
 /*
 * ispellchecker.h, Author: Fabian Stock (AiwendilH@googlemail.com)
 *
-* Copyright (C) 2001-2011 Atomic Blue (info@planeshift.it, http://www.atomicblue.org) 
+* Copyright (C) 2001-2011 Atomic Blue (info@planeshift.it, http://www.atomicblue.org)
 *
 *
 * This program is free software; you can redistribute it and/or
@@ -27,20 +27,25 @@
 struct iSpellChecker : public virtual iBase
 {
     SCF_INTERFACE(iSpellChecker, 1, 0, 0);
-    
+
     /** adds a word to the personal dictionary
+     * @param newWord. The word that should be added. This will be added temporarly to the first available dictionary and to the personal custom words list
      */
     virtual void addWord(csString newWord) = 0;
     /** get an array with words of the personal dictionary
+     * @return An array of all words from the personal dictionary
      */
     virtual const csArray<csString>& getPersonalDict() = 0;
     /** clears the whole personal dictionary
      */
     virtual void clearPersonalDict() = 0;
-    /** are any dictionaries loaded?     
-     */    
+    /** are any dictionaries loaded?
+     * @return true if at least one dictionary was loaded otherwise false
+     */
     virtual bool hasDicts() = 0;
     /** is the given word in one of the dictionaries?
+     * @param wordToCheck The word we want to check for a typo
+     * @return True if the words was found in at least one of the dictionaries.
      */
     virtual bool correct(csString wordToCheck) = 0;
 };
