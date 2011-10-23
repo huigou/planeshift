79556005758bef3c7915477bc3570e9ead85adc7
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2011-10-23 11:49:24 +0000

    bumped updater version to 2.97

--- trunk/src/pslaunch/updaterengine.h
+++ trunk/src/pslaunch/updaterengine.h
@@ -34,13 +34,13 @@
 #include "util/singleton.h"
 
 /* To be incremented every time we want to make an update. */
-#define UPDATER_VERSION 2.96
+#define UPDATER_VERSION 2.97
 
 struct iConfigManager;
 struct iVFS;
 
 #define UPDATER_VERSION_MAJOR 2
-#define UPDATER_VERSION_MINOR 96
+#define UPDATER_VERSION_MINOR 97
 
 class InfoShare
 {
