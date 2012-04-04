739a579cf45afbb2ae63956006b6f64e3aa17b33
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2012-04-04 20:14:49 +0000

    correctly handle end of dialog

--- trunk/src/client/gui/pawsnpcdialog.cpp
+++ trunk/src/client/gui/pawsnpcdialog.cpp
@@ -39,7 +39,6 @@
  * -fix handling of free text and questions
  * -fix the graphics of the chat bubbles generically
  * -add support for numkey/enter action on the list version
- * -fix the spelling of the widget name
  * -need to move the catch of npc text from chatbubbles to an inner implementation here
  *  so the two classes aren't interwingled
  */
@@ -83,6 +82,8 @@ void pawsNpcDialogWindow::Draw()
     {//let this dialog invisible after 12 secs when npc finishes his speech
      //equal to the long chatbubble longphrase lenght for now.
         speechBubble->Hide();
+        FindWidget("FreeBubble")->Show();
+        psengine->GetCmdHandler()->Execute("/npcmenu");
         ticks = 0;
     }
 
@@ -157,6 +158,7 @@ bool pawsNpcDialogWindow::OnKeyDown(utf32_char keyCode, utf32_char key, int modi
                         }
                         DisplayTextBubbles(text.GetData());
                     }
+                    textBox->Clear();
                 }
 
                 break;
