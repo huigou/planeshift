b067cbba668577a3d3c8302af20ffd80c02b315e
woden_ <woden_@2752fbe2-5038-0410-9d0a-88578062bcef>
2012-04-08 20:48:14 +0000

    Small version number display fix.

--- src/pslaunch/pslaunch.cpp
+++ src/pslaunch/pslaunch.cpp
@@ -415,14 +415,14 @@ int main(int argc, char* argv[])
 
     if(help)
     {
-        printf("PlaneShift Updater Version %d.%d for %s.\n"
+        printf("PlaneShift Updater Version %1.2f for %s.\n"
                 "Launcher and updater for Planeshift\n\n"
                 "pslaunch [--help] [--console] [--repair] [--switch]\n\n"
                 "--help      Displays this help dialog\n"
                 "--console   Run updater without the GUI\n"
                 "--switch    Switch active updater mirror\n"
                 "--repair    Check for any problems and prompt to repair them\n",
-                UPDATER_VERSION_MAJOR, UPDATER_VERSION_MINOR, (new Config())->GetPlatform());
+                UPDATER_VERSION, (new Config())->GetPlatform());
     }
     else if(console)
     {
@@ -432,7 +432,7 @@ int main(int argc, char* argv[])
         // Initialize updater engine.
         UpdaterEngine* engine = new UpdaterEngine(args, updater->GetObjectRegistry(), "pslaunch");
 
-        printf("\nPlaneShift Updater Version %d.%d for %s.\n\n", UPDATER_VERSION_MAJOR, UPDATER_VERSION_MINOR, engine->GetConfig()->GetCurrentConfig()->GetPlatform());
+        printf("\nPlaneShift Updater Version %1.2f for %s.\n\n", UPDATER_VERSION, engine->GetConfig()->GetCurrentConfig()->GetPlatform());
 
         // Run the update process!
         updater->RunUpdate(engine);
