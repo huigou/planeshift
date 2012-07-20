c34dbe7587696e19cde6630999f2ac5f295eea00
lpancallo <lpancallo@2752fbe2-5038-0410-9d0a-88578062bcef>
2012-07-20 15:06:25 +0000

    removed extra text

--- src/server/database/mysql/math_scripts.sql
+++ src/server/database/mysql/math_scripts.sql
@@ -274,36 +274,6 @@ ResultQuality = ResultQuality*300;
 
 ");
 
-Total = Distance * Probability * Quality * Skill + 0.2;
-
-SkillFactor = PlayerSkill / RequiredSkill;
-if (SkillFactor>6) {
-  SkillFactor = 6;
-}
-if (SkillFactor<1) {
-  SkillFactor = 1;
-}
-
-ToolFactor = 0.2*ToolQuality / RequiredToolQuality;
-if (ToolFactor>2) {
-  ToolFactor = 2;
-}
-if (ToolFactor<1) {
-  ToolFactor = 1;
-}
-
-RandomFactor = (0.4*rnd(1))+0.8;
-ResultQuality = 50*SkillFactor*ToolFactor*RandomFactor;
-if (ResultQuality>300) {
-  ResultQuality = 300;
-}
-if (ResultQuality<50) {
-  ResultQuality = 50;
-}
-
-ResultQuality = floor(ResultQuality);
-
-
 INSERT INTO math_scripts VALUES( "Calc Item Merchant Price Buy", "Result = ItemPrice - CharData:GetSkillValue(47)/10;");
 
 INSERT INTO math_scripts VALUES( "Calc Item Merchant Price Sell", "Result = ItemPrice + CharData:GetSkillValue(47)/10;");
