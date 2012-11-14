65f1cd4b6c626a67c0c0b06764f753527b05f1ba
lpancallo <lpancallo@2752fbe2-5038-0410-9d0a-88578062bcef>
2012-11-14 05:06:04 +0000

    fixed clamp error

--- src/server/usermanager.cpp
+++ src/server/usermanager.cpp
@@ -473,9 +473,9 @@ void UserManager::CalculateComparativeDifference(psCharacter* myCharData, psChar
     int magicalDiffValue = theirMagicalLevel - myMagicalLevel;
     // Clamp within the allowed range (0-6) considering difference from the median (3)
     physicalDiffValue = (3+physicalDiffValue)>maxLevel?maxLevel:(3+physicalDiffValue);
-    physicalDiff = (3+physicalDiffValue)<0?0:(3+physicalDiffValue);
+    physicalDiff = physicalDiffValue<0?0:physicalDiffValue;
     magicalDiffValue = (3+magicalDiffValue)>maxLevel?maxLevel:(3+magicalDiffValue);
-    magicalDiff = (3+magicalDiffValue)<0?0:(3+magicalDiffValue);
+    magicalDiff = magicalDiffValue<0?0:magicalDiffValue;
 
     overallLevelComparison = int ((float)(2 * maxLevel + physicalDiff + magicalDiff) / 3.5f);
 }
