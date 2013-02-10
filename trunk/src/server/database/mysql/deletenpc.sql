75bdba758b53737abaf2dc01bd35366db52082e5
lpancallo <lpancallo@2752fbe2-5038-0410-9d0a-88578062bcef>
2013-02-10 21:01:27 +0000

    added sc_npc_definitions table

--- trunk/src/server/database/mysql/deletenpc.sql
+++ trunk/src/server/database/mysql/deletenpc.sql
@@ -1,15 +1,6 @@
 #
 # How to delete an NPC
 # 
-# The tables populated by an NPC are:
-#
-# npc_bad_text
-# npc_knowledge_areas
-# npc_responses
-# npc_triggers
-# objects
-# player_items
-# players
 # 
 # use this command to know which NPCs are currently loaded
 # select name from players where alive_ind='Y';
@@ -35,25 +26,9 @@ delete from npc_triggers where area=@npc_name;
 delete from item_instances where char_id_owner=@npc_id;
 delete from character_skills where character_id=@npc_id;
 delete from merchant_item_categories where player_id=@npc_id;
+delete from trainer_skills where player_id=@npc_id;
+
+#delete npcclient entries
+delete from sc_npc_definitions WHERE char_id=@npc_id;
+
 delete from characters where id=@npc_id;
-
-
-
-# NPC names currently in game
-# 
-# Rabartus Livrandar
-# Jefecra Harcrit
-# Aleena Arlavin
-# Ri\'Miyan Sulka
-# Thorian Gronk
-# Jayose
-# Lori Try\'llyn
-# Gayla Grotemey
-# Jeyarp Grotemey
-# Rinna Voladrand
-# Boralis Voladrand
-# Dur\'ok
-
-# Currently not used
-# Gurard Anvilforce
-# Maynard
