a44d7dfc13408fd6685f2bb29e66e64298dd1ef4
lpancallo <lpancallo@2752fbe2-5038-0410-9d0a-88578062bcef>
2013-07-31 22:35:40 +0000

    Fixed active bar icon display by Eredin.

--- trunk/src/common/net/messages.h
+++ trunk/src/common/net/messages.h
@@ -3711,17 +3711,14 @@ public:
             if( image.Length()>0 )        //<--
             {
                 msgSize += image.Length() +1;
-//printf( "psGUIActiveMagicMessage::psGUIActiveMagicMessage got image '%s'\n", image.GetData());
             }
             else
             {
-//printf( "psGUIActiveMagicMessage::psGUIActiveMagicMessage got zero-length image\n");
             }
         }
         else
         {
             msgSize += sizeof(uint8_t);
-//printf( "psGUIActiveMagicMessage::psGUIActiveMagicMessage got no image\n" );
         }
 
         //msg.AttachNew(new MsgEntry(sizeof(bool) + sizeof(uint8_t) + sizeof(int32_t) + name.Length() + image.Length() + 2));
@@ -3741,7 +3738,6 @@ public:
             msg->Add((uint8_t)0);
 
         valid = !(msg->overrun);
-//printf( "psGUIActiveMagicMessage::psGUIActiveMagicMessage sending message ( %i, %i, %i, %s, %s)\n", clientNum, cmd, type, name?name.GetData():"N/A", image?image.GetData():"N/A" );
     }
 
     /// Crack this message off the network.
@@ -3752,11 +3748,9 @@ public:
         name = message->GetStr();          //if there was no name when the message was sent, this should read a null string
         image = message->GetStr();         //if there was no name when the message was sent, this should read a null string
         valid = true;
-//printf( "psGUIActiveMagicMessage::psGUIActiveMagicMessage receiving message ( %i, %i, %s, %s)\n", command, type, name.GetData(), image.GetData() );
     }
 
     PSF_DECLARE_MSG_FACTORY();
-
     /**
      *  Converts the message into human readable string.
      *
