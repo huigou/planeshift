36795033fe05596785dd0784ee933fe581e99d8e
whacko88 <whacko88@2752fbe2-5038-0410-9d0a-88578062bcef>
2013-09-01 20:23:07 +0000

    implemented notes and musical contexts

--- src/common/music/scoreelements.h
+++ src/common/music/scoreelements.h
@@ -24,6 +24,8 @@
 //====================================================================================
 // Crystal Space Includes
 //====================================================================================
+#include <cssysdef.h>
+#include <csutil/hash.h>
 
 //====================================================================================
 // Project Includes
@@ -32,21 +34,181 @@
 //====================================================================================
 // Local Includes
 //====================================================================================
+#include "musicutil.h"
 
 //------------------------------------------------------------------------------------
 // Forward Declarations
 //------------------------------------------------------------------------------------
+#define UNDEFINED_MEASURE_ATTRIBUTE -100
 
+struct ScoreContext;
+
+using namespace psMusic;
+
+/**
+ * \addtogroup common_music
+ * @{ */
+
+//--------------------------------------------------
 
 /**
  * A single note in a musical score.
  */
 class Note
 {
+public:
+    /**
+     * Used to keep track of previous altered notes in the current measure. Hides the
+     * implementation of note context updating to other classes.
+     */
+    class NoteContext
+    {
+    public:
+        /**
+         * Constructor.
+         */
+        NoteContext();
+
+        /**
+         * Get an eventual accidental that previously appeared in the same measure.
+         *
+         * @param note The note that should be checked.
+         * @return The accidental in case there where previous altered notes or
+         * NO_ACCIDENTAL if none.
+         */
+        Accidental GetPreviousAccidental(const Note &note) const;
+
+        /**
+         * Update the list of previous accidental if the given note has any.
+         *
+         * @param note The current played note.
+         */
+        void UpdateContext(const Note &context);
+
+        /**
+         * Empty the list of previously altered note.
+         */
+        void ResetContext();
+
+    private:
+        /**
+        * Previous notes with a written accidental. Accidentals are indexed by note octave
+        * and name. Must be resetted at every measure and updated at every note.
+        */
+        csHash<csHash<Accidental, char>, int> prevAccidentals;
+    };
+
+    /**
+     * Constructor.
+     *
+     * @param name Name of the note (C, D, E, F, G, A or B). Must be uppercase.
+     * @param octave Octave number. Octave number 4 is the one that contains the central
+     * C on the piano.
+     * @param accidental The accidental with which the note is written on the score. This
+     * is not related to the tonality of the piece but only on its representation. In
+     * other words the variable accidental must be different than UNALTERED if and only if
+     * an actual accidental is written on the musical score.
+     */
+    Note(char name, int octave, Accidental accidental);
+
+    /**
+     * Get the note name.
+     *
+     * @return The uppercase name of the note (C, D, E, F, G, A or B).
+     */
+    char GetName() const { return name; }
+
+    /**
+     * Get the note octave number.
+     *
+     * @return The octave number of this note. Octave number 4 is the one that contains
+     * the central C on the piano.
+     */
+    int GetOctave() const { return octave; }
+
+    /**
+     * Get the accidental of the played note. This, depending on the tonality, may not be
+     * the same as the written accidental.
+     *
+     * @param context The context where tonality and previous accidentals are kept.
+     * @return The accidental with which the note must be played in the context of the
+     * tonality of the piece. An eventual accidental written on the score have the
+     * priority.
+     */
+    Accidental GetPlayedAccidental(const ScoreContext &context) const;
+
 private:
-    char name;  ///< Name of the note (C, D, E, F, G, A or B).
-    short int octave;   ///< Octave number. C4 is the central C of the piano.
-    Accidental accidental;   ///< The accidental with which the note is represented.
+    char name; ///< Uppercase name of the note (C, D, E, F, G, A or B).
+    int octave; ///< Octave number. C4 is the central C on piano.
+
+    /**
+     * The accidental with which the note is written on the score. It does not have
+     * anything to do with the tonality. With this representation, one does not have to
+     * modify all the notes in the score when changing the tonality of the piece.
+     */
+    Accidental accidental;
 };
+
+//--------------------------------------------------
+
+class Chord
+{
+};
+
+//--------------------------------------------------
+
+class Measure
+{
+public:
+    /**
+     * Keep general attributes that can change from a measure to another like key signature,
+     * beats and tempo. Some attributes can be undefined. This means that the specific
+     * attribute takes the same value as in the previous measure. Undefined attributes take
+     * the value UNDEFINED_MEASURE_ATTRIBUTE.
+     */
+    struct MeasureAttributes
+    {
+        int tempo; ///< Quarter notes per minute (BPM).
+        int beats; ///< Numerator of the time signature.
+        int beatType; ///< Denominator of the time signature.
+
+        /**
+         * Unit of measure for notes duration in terms of divisions per quarter.
+         */
+        int quarterDivisions;
+
+        /**
+         * Number of accidentals in the key signature. Positive for sharps, negative for
+         * flats.
+         */
+        int fifths;
+
+        MeasureAttributes();
+    };
+};
+
+//--------------------------------------------------
+
+/**
+ * Keep track of previous accidentals in the same measure and attributes of previous
+ * measures when they are not specified in the current one. This struct make sense only
+ * when it refers to a specific point in the musical score.
+ */
+struct ScoreContext
+{
+    // TODO how to handle repeats for measure attributes?
+
+    /**
+     * Used to keep track of previous accidentals in the same measure.
+     */
+    Note::NoteContext noteContext;
+
+    /**
+     * Attributes specified in the score up to now. Must be updated at every measure.
+     */
+    Measure::MeasureAttributes prevAttributes;
+};
+
+/** @} */
 
 #endif // SCORE_ELEMENTS_H
