7f789c48f44ec69366e999844cf850fdb4a31fc9
ralphcampbell <ralphcampbell@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-04-23 16:19:29 +0000

    Prevent multiple attacks from being queued. The alternative fix would be delete psCombatAttackGameEvent from the CS event queue when the attacker stops attacking but that is a bit more complicated than just keeping a count of how many events are queued.

--- src/server/gem.cpp
+++ src/server/gem.cpp
@@ -2177,7 +2177,7 @@ gemActor::gemActor(GEMSupervisor* gemsupervisor, CacheManager* cachemanager, Ent
                    float rotangle,
                    int clientnum) :
     gemObject(gemsupervisor,entitymanager,cachemanager,chardata->GetCharFullName(),factname,myInstance,room,pos,rotangle,clientnum),
-    psChar(chardata), mount(NULL), DRcounter(0), forceDRcounter(0), lastDR(0), lastV(0), lastSentSuperclientPos(0, 0, 0),
+    psChar(chardata), mount(NULL), attack_cnt(0), DRcounter(0), forceDRcounter(0), lastDR(0), lastV(0), lastSentSuperclientPos(0, 0, 0),
     lastSentSuperclientInstance(-1), activeReports(0), isFalling(false), invincible(false), visible(true), viewAllObjects(false),
     movementMode(0), isAllowedToMove(true), atRest(true), player_mode(PSCHARACTER_MODE_PEACE), spellCasting(NULL), workEvent(NULL),
     activeMagic_seq(0), pcmove(NULL), nevertired(false), infinitemana(false), instantcast(false), safefall(false), givekillexp(false),
