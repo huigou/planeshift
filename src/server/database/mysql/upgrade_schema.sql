a58ab2f3e8fefe77fb99d2e530c6eb096f960026
ralphcampbell <ralphcampbell@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-05-04 21:15:37 +0000

    Update the database version and upgrade_schema.sql

--- src/server/database/mysql/upgrade_schema.sql
+++ src/server/database/mysql/upgrade_schema.sql
@@ -1858,6 +1858,18 @@ SOURCE attacks.sql;
 SOURCE attack_types.sql;
 UPDATE `server_options` SET `option_value`='1278' WHERE `option_name`='db_version';
 
+# Use item_stats.id and weapon_types.id for attack_types.weaponID and weaponType.
+DROP table attack_types;
+SOURCE attack_types.sql;
+INSERT INTO command_group_assignment VALUES( "/version", 30 );
+INSERT INTO command_group_assignment VALUES( "/version", 25 );
+INSERT INTO command_group_assignment VALUES( "/version", 24 );
+INSERT INTO command_group_assignment VALUES( "/version", 23 );
+INSERT INTO command_group_assignment VALUES( "/version", 22 );
+INSERT INTO command_group_assignment VALUES( "/version", 21 );
+INSERT INTO command_group_assignment VALUES( "/version", 10 );
+UPDATE `server_options` SET `option_value`='1279' WHERE `option_name`='db_version';
+
 # Insert your upgrade before this line. Remember when you set a new db_version
 # to update the server_options.sql file and update psserver.cpp as well.
 # This to ensure that everything is working if you use the create_all.sql to
