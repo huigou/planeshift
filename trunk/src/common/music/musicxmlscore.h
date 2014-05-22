d4a7f9f1f882457a74b88df18926a5f3b30a3368
whacko88 <whacko88@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-05-22 23:01:43 +0000

    implemented MusicXMLNote

--- trunk/src/common/music/musicxmlscore.h
+++ trunk/src/common/music/musicxmlscore.h
@@ -1,7 +1,7 @@
 /*
  * musicxmlscore.h, Author: Andrea Rizzi <88whacko@gmail.com>
  *
- * Copyright (C) 2001-2013 Atomic Blue (info@planeshift.it, http://www.atomicblue.org) 
+ * Copyright (C) 2001-2014 Atomic Blue (info@planeshift.it, http://www.atomicblue.org) 
  *
  *
  * This program is free software; you can redistribute it and/or
@@ -32,10 +32,36 @@
 //====================================================================================
 // Local Includes
 //====================================================================================
+#include "scoreelements.h"
 
 //------------------------------------------------------------------------------------
 // Forward Declarations
 //------------------------------------------------------------------------------------
 
+/**
+* \addtogroup common_music
+* @{ */
+
+/**
+ * Extends the class Note by adding the ability to parse MusicXML.
+ */
+class MusicXMLNote: public Note
+{
+public:
+    /**
+     * Load the note step, note and accidental from the given <pitch> node.
+     *
+     * @param pitchNode a valid pointer to the <pitch> node containing the definition of
+     * the note in MusicXML syntax.
+     * @return false if the given node syntax is wrong, true otherwise.
+     */
+    bool LoadXML(csRef<iDocumentNode> &pitchNode);
+
+    /**
+     * Convert the note to XML.
+     * @return the string containing the note definition in MusicXML syntax.
+     */
+    csString ToXML();
+};
 
 #endif // MUSIC_XML_SCORE_H
--- trunk/src/common/music/musicxmlscore.hpp
+++ trunk/src/common/music/musicxmlscore.hpp
@@ -1,7 +1,7 @@
 /*
- * musicxmlscore.cpp, Author: Andrea Rizzi <88whacko@gmail.com>
+ * musicxmlscore.hpp, Author: Andrea Rizzi <88whacko@gmail.com>
  *
- * Copyright (C) 2001-2013 Atomic Blue (info@planeshift.it, http://www.atomicblue.org) 
+ * Copyright (C) 2001-2014 Atomic Blue (info@planeshift.it, http://www.atomicblue.org) 
  *
  *
  * This program is free software; you can redistribute it and/or
