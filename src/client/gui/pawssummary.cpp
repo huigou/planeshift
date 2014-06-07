6e3cd16b979b59818e40232df57cc6af20ce59ba
ralphcampbell <ralphcampbell@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-06-07 06:16:27 +0000

    Set the main player name when creating a new charater or the loading screen won't go away.

--- src/client/gui/pawssummary.cpp
+++ src/client/gui/pawssummary.cpp
@@ -76,7 +76,7 @@ bool pawsSummaryWindow::PostSetup()
 void pawsSummaryWindow::Update()
 {
     pawsTextBox* label_name = (pawsTextBox*) FindWidget("label_name");
-    label_name->SetText(createManager->GetName().GetData());
+    label_name->SetText(createManager->GetName());
 
     csString textString;
     csString lifeString;
