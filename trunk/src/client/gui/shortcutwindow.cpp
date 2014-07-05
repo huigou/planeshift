5bf3e9bced6db3853b8ae7580a1cc0e118314903
ralphcampbell <ralphcampbell@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-07-05 21:12:37 +0000

    Only call SetCurrentValue() for the scrollbar when actually changing the current value or else we go into an infinite loop.

--- trunk/src/client/gui/shortcutwindow.cpp
+++ trunk/src/client/gui/shortcutwindow.cpp
@@ -681,15 +681,17 @@ bool pawsShortcutWindow::OnScroll(int direction, pawsScrollBar* widget)
         if( pos>NUM_SHORTCUTS-1 )
         {
             pos=NUM_SHORTCUTS-1;
+            widget->SetCurrentValue( pos );
+            MenuBar->ScrollToPosition( pos );
         }
         else if( pos<0 )
         {
             pos=0.0;
+            widget->SetCurrentValue( pos );
+            MenuBar->ScrollToPosition( pos );
         }
-        widget->SetCurrentValue( pos );
-        MenuBar->ScrollToPosition( pos );
     }
- return true;
+    return true;
 }
 
 void pawsShortcutWindow::ResetEditWindow()
