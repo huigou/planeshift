b6e42dbd81c2ada427e2323e0b27f36436a7a0a4
ralphcampbell <ralphcampbell@2752fbe2-5038-0410-9d0a-88578062bcef>
2015-03-01 07:12:53 +0000

    Call csInitializer::DestroyApplication() instead of CS_STATIC_VARIABLE_CLEANUP to clean up after csInitializer::CreateEnvironment().

--- trunk/src/tools/ccheck/ccheck.cpp
+++ trunk/src/tools/ccheck/ccheck.cpp
@@ -447,6 +447,6 @@ int main(int argc, char** argv)
     CCheck* ccheck = new CCheck(object_reg);
     ccheck->Run();
     delete ccheck;
-    CS_STATIC_VARIABLE_CLEANUP
+    csInitializer::DestroyApplication(object_reg);
     return 0;
 }
