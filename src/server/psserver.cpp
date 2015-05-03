6f3456a6a7255fd77ece07cbeb392d5e239d8141
lpancallo <lpancallo@2752fbe2-5038-0410-9d0a-88578062bcef>
2015-05-03 14:11:46 +0000

    Updated to 1280 db version

--- src/server/psserver.cpp
+++ src/server/psserver.cpp
@@ -95,7 +95,7 @@
 #include "workmanager.h"
 
 // Remember to bump this in server_options.sql and add to upgrade_schema.sql!
-#define DATABASE_VERSION_STR "1279"
+#define DATABASE_VERSION_STR "1280"
 
 
 psCharacterLoader psServer::CharacterLoader;
