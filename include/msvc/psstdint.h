6aaea4833157622e2e7e5f8f8ed9c5e4b6cd7484
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-03-28 15:55:14 +0000

    Added a check to remove a redefined typedef for msvc2015.

--- include/msvc/psstdint.h
+++ include/msvc/psstdint.h
@@ -7,7 +7,15 @@
 
 // Remove *_t definitions to be consistent with CS types
 
+// This "should" actually be true for all c++2011 implementation. Additional versions need to be added if other OS/tool chains break on the redefinition of int8_t
+// We do not print this typedef for msvc 2015.
+#ifdef _MSC_VER
+#if (_MSC_VER < 1900)
 typedef int8	int8_t;
+#endif
+#else
+typedef int8	int8_t;
+#endif
 typedef uint8	uint8_t;
 typedef int16	int16_t;
 typedef uint16	uint16_t;
